f0349e2e6959c5edbe19b3e7f1f6936fdb01ee61
William Dubose <wmdubose@gmail.com>
2016-02-14 03:21:59 +0000

    added contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730185251) do
+ActiveRecord::Schema.define(version: 20160214031707) do
+
+  create_table "contacts", force: true do |t|
+  end
 
   create_table "conversations", force: true do |t|
     t.integer  "sender_id"
