c09e5a12fa72059fd91d8fd7217a963ca30364a3
William Dubose <wmdubose@gmail.com>
2016-02-15 00:50:25 +0000

    Added signup rights link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
     def index
+        @basic_right = Right.find(1)
+        @employee_right = Right.find(2)
+        @VIP_right = Right.find(3)
     end    
     def about
 
