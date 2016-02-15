29c8124af6514b898b5aaa1a2623d7cb43a01c63
William Dubose <wmdubose@gmail.com>
2016-02-15 03:07:40 +0000

    Added profile actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
     def index
         @basic_right = Right.find(1)
         @employee_right = Right.find(2)
-        @VIP_right = Right.find(3)
     end    
     def about
 
