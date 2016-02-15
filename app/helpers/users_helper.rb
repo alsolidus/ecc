d62cfee88b060da1e8ccad2c1305d8503b9d5166
William Dubose <wmdubose@gmail.com>
2016-02-15 05:54:57 +0000

    show updates

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,13 @@ module UsersHelper
   def conversation_interlocutor(conversation)
     conversation.recipient == current_user ? conversation.sender : conversation.recipient
   end
+  def job_title_icon
+    if @user.profile.job_title == "ECC"
+        "<i class='fa fa-code'></i>".html_safe
+      elsif @user.profile.job_title == "NOC"
+        "<i class='fa fa-lightbulb-o'></i>".html_safe
+        elsif @user.profile.job_title == "employee"
+        "<i class='fa fa-dollar'></i>".html_safe
+    end
+end
 end
