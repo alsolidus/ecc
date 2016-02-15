099fc2c609c91b805a74d8cccdc9160814ff7f2c
William Dubose <wmdubose@gmail.com>
2016-02-15 23:45:36 +0000

    update for heroku

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+# For errors like ActiveRecord::RecordNotFound
+require "active_record"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
